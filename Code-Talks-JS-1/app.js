620983df3b534c44a4841dbf0e3c27f62cf062f6
Myles <mylesastoria2@gmail.com>
2023-05-08 10:24:36 -0400

    first commit

---
+++ Code-Talks-JS-1/app.js
@@ -0,0 +1,26 @@
+// prints to the console 'I love pizza' 5 times
+console.log(`I love pizza`);
+console.log(`I love pizza`);
+console.log(`I love pizza`);
+console.log(`I love pizza`);
+console.log(`I love pizza`);
+
+
+// for loops allow us to repeat the same action as many times as we want!
+// instead of printing "i love pizza" 5 times individually, a for loop is more efficient.
+
+
+const n = 5; 
+// variable n is set to the value of 5
+
+for (let i = 1; i <= n; i++) {
+// initialize for loop by setting variable i to 1
+// 1 will be the beginning of the loop
+// i <= n is the condition; so the loop will only run as long as i is less than or equal to n
+// i++ is the counter; each time the loop runs the counter will increase the value of i by 1 until the condition is met.
+    console.log(`I love pizza`);
+// prints "I love pizza" to the consoe 5 times
+}
+
+
+
